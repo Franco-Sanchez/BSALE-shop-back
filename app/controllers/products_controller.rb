7bb01bb7e85dcd2eafdd50a025f2b1942f72ab60
Franco <francorsr98@gmail.com>
2021-04-19 14:55:44 -0500

    Updated endpoint to search for a product by name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   # GET /products
   def index
-    render json: Product.render_products
-  end
-
-  # GET /products/:name
-  def search
-    render json: Product.search_products(params[:name])
+    name = params[:name]
+    if name
+      render json: Product.search_products(name)
+    else
+      render json: Product.render_products
+    end
   end
 end
